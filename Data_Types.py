5733d16d6c578d31775334cc01bad69493e44252
EtienneSparks1 <s5705798@bourenouth.ac.uk>
2024-10-14 15:24:39 +0100

    Practising with tuples

--- Data_Types.py
+++ Data_Types.py
@@ -1,3 +1,4 @@
+'''
 my_list = [15, 23, 32, 46]
 
 print(my_list[0])
@@ -8,3 +9,11 @@ result = my_list[0] + my_list[-1]
 my_list.append(result)
 
 print(my_list)
+'''
+
+tuples_list = [200, 15, "Programming", 1+2, "Lab 3"]
+
+print(len(tuples_list))
+tuples_list.append(5.25)
+
+print(tuples_list)
