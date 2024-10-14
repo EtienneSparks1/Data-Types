f3ac040ca3a8674e465f4a0fcaf07630feaf3d30
EtienneSparks1 <s5705798@bourenouth.ac.uk>
2024-10-14 16:55:13 +0100

    Reverse list

--- reverse_list.py
+++ reverse_list.py
@@ -20,3 +20,9 @@ def reverse_list(my_list):
     # Complete your code here...
     
     return my_list
+
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+reversed_list = list(reversed(my_list))
+print(reversed_list)
